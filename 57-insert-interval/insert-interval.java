b8f9c0b7ee5c074ecdec952bd50cafc71bc475db
Ripal <32412354+riipal@users.noreply.github.com>
2026-04-08 15:52:57 -0700

    Time: 2 ms (27.88%) | Memory: 47 MB (63.24%) - LeetSync

---
+++ 57-insert-interval/insert-interval.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> res = new ArrayList<>();
+        int i = 0;
+        //Store intervals before overlap
+        while(i < intervals.length && intervals[i][1] < newInterval[0]) {
+            res.add(intervals[i]);
+            i++;
+        }
+
+        //Merge overlapping intervals
+        while(i < intervals.length && newInterval[1] >= intervals[i][0]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]); 
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.add(newInterval);
+
+        //Append remaining
+        while(i < intervals.length) {
+            res.add(intervals[i]);
+            i++;
+        }
+        return res.toArray(new int[res.size()][]);
+
+    }
+}
